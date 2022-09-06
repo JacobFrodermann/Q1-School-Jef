35620dab79797740bcc531047e0b0df4a713b30e
Red-S2 <66247321+Red-S2@users.noreply.github.com>
2022-09-06 22:51:02 +0200

    wored in the eveing fixed ui

--- display/Contact Mng/src/components/Contact.vue
+++ display/Contact Mng/src/components/Contact.vue
@@ -4,20 +4,39 @@ interface Contact {
     Number:Number,
 }
 export default {
+    data(){return {
+        hidden:true,
+    }},
     props:{
         contactIn:Object,
     }
 }
 </script>
 <template>
-    <div>
-        <h1>{{contact.Name}}</h1>
-        <h3>Number: {{contact.Number}}</h3>
+    <div @mouseover="hidden=false" @mouseleave="hidden=true" id="container">
+        <h1>{{contactIn.Name}}</h1>
+        <h3 v-if="contactIn.Number">Number: {{contactIn.Number}}</h3>
+        <h3 v-if="contactIn.Address">Address: {{contactIn.Address}}</h3>
+        <div id="button" v-if="!hidden">
+            <img src="../assets/delete.svg"/>
+        </div>
     </div>
 </template>
 <style scoped>
-    div {
+    #container {
         border: solid;
         border-radius: 10px;
+        margin: 5px;
+    }
+    h1, h3 {
+        margin: 5px;
+    }
+    h1{
+        text-align: center;
+    }
+    #button {
+        display: relative;
+        left:175px;
+        top:-40px;
     }
 </style>
